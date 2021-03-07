f4da81ab7ddb8b19898e9faa0030477ee009cc68
vittojanapin <vittojanapin@outlook.com>
2021-03-07 13:57:44 +0000

    ✈ Added the output function and the board to work with

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+const char secret[3] = {'R', 'G', 'B'}; //the set of colors to guess
+
+char board[7][7] = {
+    {'1' , '_', '_', '_', '0', '0', '0' } ,
+    {'2' , '_', '_', '_', '0', '0', '0' } ,
+    {'3' , '_', '_', '_', '0', '0', '0' } ,
+    {'4' , '_', '_', '_', '0', '0', '0' } ,
+    {'5' , '_', '_', '_', '0', '0', '0' } ,
+    {'6' , '_', '_', '_', '0', '0', '0' } ,
+    {'7' , '_', '_', '_', '0', '0', '0' } ,
+}; //array containing of the given characters (this is what we will alter)
+
+void board_out (){    //output of the board (note: try it out to see how it looks)
+    for(int i = 0 ; i < 7; i++){
+        cout << "\t -------------------"  << endl; 
+        cout << "\t | ";
+        for(int k = 0 ; k < 7 ; k++){
+            cout << board[i][k] << ' ';
+        }
+        cout << " |" << endl;
+    }
+}
+int main(){ //main function should only contain the main loop (for turns) 
+    cout << "Mastermind Game!" << endl;
+    board_out();
 }
